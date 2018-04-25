1de421c98a3cc920026eb32b504b866fc8fd2b7d
g00341965 <35809401+g00341965@users.noreply.github.com>
2018-04-25 14:09:39 +0100

    Add files via upload

--- js/convert.js
+++ js/convert.js
@@ -4,19 +4,42 @@ function euroToPounds(){
   
 
   var pounds = amount * 0.86;
-  var usd = amount * 1.06;
-  var yen = amount * 121.12;
-  var bitcoin = amount * 0.00082;
-  
-  
   var message = amount + ' euros converts to ' + pounds + ' sterling.';
-  var message = amount + ' euros converts to ' + usd; 
-  var message = amount + ' euros converts to ' + yen; 
-  var message = amount + ' euros converts to ' + bitcoin; 
   
-
-  
-  console.log(message);
-  doc.getElementById("resultsentence").innerHTML = message;
 
 }
+
+fucntion euroToUsd(){
+	var amount = parseInt(document.getElementById("value1").value);
+	
+	var usd = amount * 1.06;
+	var message2 = amount + ' euros converts to ' usd;
+	
+	
+}	
+
+function euroToYen(){
+	
+	var amount = parseInt(document.getElementById("value1").value);
+	
+	var yen = amount * 121.12;
+	var message3 = amount + ' euros converts to ' + yen; 
+	
+}
+
+function euroToBitcoin(){
+	
+	var amount = parseInt(document.getElementById("value1").value);
+	
+	var bitcoin = amount * 0.00082;
+	var message = amount + ' euros converts to ' + bitcoin; 
+	
+	
+}
+
+
+console.log(message);
+doc.getElementById("resultsentence").innerHTML = message;
+	
+	
+	
